1d94e5d628bed0003ce880b1e9675bb71ba2108a
Lucie Yarish <lucieyarish@hotmail.com>
2024-03-06 06:20:13 +0100

    Create Button component for generating memes

--- src/layout/Meme.jsx
+++ src/layout/Meme.jsx
@@ -1,10 +1,13 @@
 import './Meme.css';
 import Form from '../components/Form';
+import Button from '../components/Button';
 
 const Meme = () => {
+  const btnText = 'Get a new meme image';
   return (
     <main className="main-container">
       <Form />
+      <Button text={btnText} />
     </main>
   );
 };
