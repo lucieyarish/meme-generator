c16d854283bfb0f34edc8468875c75c48880a417
Lucie Yarish <lucieyarish@hotmail.com>
2024-03-06 07:38:33 +0100

    Add data with meme URLs; get random URL on btn click - WIP

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
 import './Button.css';
+import memesData from '../data/memesData.js';
 
 const Button = (props) => {
-  return <button className="btn">{props.text}</button>;
+  const getRandomNumber = () => {
+    return Math.floor(Math.random() * memesData.data.memes.length);
+  };
+  const getRandomMemeImg = () => {
+    const memesArray = memesData.data.memes;
+    const randomIndex = getRandomNumber();
+    return memesArray[randomIndex].url;
+  };
+
+  return (
+    <button className="btn" type="button" onClick={getRandomMemeImg}>
+      {props.text}
+    </button>
+  );
 };
 
 export default Button;
